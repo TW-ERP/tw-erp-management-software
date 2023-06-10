8edd20621e56f221bfc831b12fcbed5a5e7c325a
Shuai Shao <shuai-shao@outlook.com>
2023-06-10 11:57:08 -0700

    add server functionality

---
+++ Server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+/* handle parsing request body */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// statically serve everything in the build folder on the route '/build'
+app.use('/build', express.static(path.join(__dirname, '../build')));
+
+// serve index.html on the route '/'
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+// handle unknown paths
+app.use((req, res) => res.sendStatus(404));
+
+// global error handler
+app.use((err, req, res, next) => {
+  //Define a default error object
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occured' },
+  };
+  // define an errorObj to combine new errors
+  const errObj = Object.assign(defaultErr, err);
+  console.log('Error: ', errObj.log);
+  // return to the client the status and error message
+  return res.status(errObj.status || 500).send(errObj.message);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = app;
