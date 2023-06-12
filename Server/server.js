4c7d555a18a2bf0b82ccc94c74a98aa087dbc3d6
Donald Twiford <donaldptwiford@gmail.com>
2023-06-12 16:07:48 -0700

    complete product query setup for backend requests Co-authored-by: Michael Liu KwonJiyongGD@github.com

--- Server/server.js
+++ Server/server.js
@@ -19,6 +19,7 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
+
 // still need a place to redirect after successful login
 app.post('/login', userController.verifyUser, (req, res) => {
   if (res.locals.result === true) {
